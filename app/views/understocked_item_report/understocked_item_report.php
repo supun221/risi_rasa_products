d4aea0e8209654460b923d29f1370ccd6fe6506d
BinodGayasri <binodgayasri2001@gmail.com>
2025-09-09 15:33:11 +0530

    minor fix

--- app/views/understocked_item_report/understocked_item_report.php
+++ app/views/understocked_item_report/understocked_item_report.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="images/logo.png">
-    <title>Eggland Super</title>
+    <title>RISI RASA PRODUCTS</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Maname&family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Yaldevi:wght@200..700&display=swap" rel="stylesheet">
@@ -136,7 +136,7 @@
 <body>
 
 <div class="report-body">
-    <h1 class="report-headline">Eggland Super</h1>
+    <h1 class="report-headline">RISI RASA PRODUCTS</h1>
     <h4 class="report-title">Understocked Items Detailed Report</h4>
     <h6 class="date"><?php echo $currentDate ?></h6>
 
